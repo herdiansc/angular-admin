40d51520b38f1fe0218f98276e90e8be05190e63
herdianovo <herdian@ovo.id>
2019-01-20 14:11:56 +0700

    Initial  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'employees',
+    component: EmployeeComponent,
+    data: { title: 'Employee List' }
+  },
+  {
+    path: 'employee-details/:id',
+    component: EmployeeDetailComponent,
+    data: { title: 'Employee Details' }
+  },
+  {
+    path: 'employee-add',
+    component: EmployeeAddComponent,
+    data: { title: 'Employee Add' }
+  },
+  {
+    path: 'employee-edit/:id',
+    component: EmployeeEditComponent,
+    data: { title: 'Employee Edit' }
+  },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeAddComponent,
+    EmployeeDetailComponent,
+    EmployeeEditComponent
   ],
   imports: [
-    BrowserModule
+  	RouterModule.forRoot(appRoutes),
+  	FormsModule,
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
